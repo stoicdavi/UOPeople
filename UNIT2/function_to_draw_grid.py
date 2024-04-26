4692cdce076a0dc90ee9339ebe741fa7bb142249
David Nanjila <nanjiladavid2@gmail.com>
2024-04-26 08:33:56 +0300

    Chore: change the name to match the units

---
+++ UNIT2/function_to_draw_grid.py
@@ -0,0 +1,7 @@
+def print_grid():
+    print("+ - - - - + - - - - +")
+    print(("|         |         |\n") *4, end="")
+    print("+ - - - - + - - - - +")
+    print(("|         |         |\n") *4, end="")
+    print("+ - - - - + - - - - +")
+print_grid()

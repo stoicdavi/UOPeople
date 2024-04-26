b5390d068a3bdfe9046ef15f629851bc5f7a42fc
David Nanjila <nanjiladavid2@gmail.com>
2024-04-26 09:02:40 +0300

    Fix: Remove uneccesary repeatition of code

--- UNIT3/mypolygon.py
+++ UNIT3/mypolygon.py
@@ -4,15 +4,7 @@ def Square(t, length):
     for i in range(4):
         t.fd(length)
         t.lt(90)
-
-        t.fd(length)
-        t.lt(90)
-
-        t.fd(length)
-        t.lt(90)
-
-        t.fd(length)
-        
-        turtle.mainloop()
+   
+    turtle.mainloop()
 length = int(input("Enter the length of the square You want to draw: "))
 Square(bob, length)

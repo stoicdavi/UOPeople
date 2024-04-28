e7e02e786530233198ef0e1af64ab7873eda3d3b
David Nanjila <nanjiladavid2@gmail.com>
2024-04-28 19:53:08 +0300

    Chore: create a code to demonstrate use of chained conditional statement

---
+++ UNIT3/exercise/chainedConditional.py
@@ -0,0 +1,10 @@
+name = 'David'
+name2 = 'Kennedy'
+name3 = 'Dan'
+na = input("Enter name: ")
+if na == 'David':
+    print(f'My name is {name}')
+elif na == 'Kennedy':
+    print(f'My name is {name2}')
+elif na == 'Dan':
+    print(f"My name is {name3}")

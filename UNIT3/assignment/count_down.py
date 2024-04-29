39c33746fd28a02954439656b812d39c53f2cc13
David Nanjila <nanjiladavid2@gmail.com>
2024-04-29 13:46:06 +0300

    feat: add a program that counts down to zero, add an if else statement to check if a positive number is entered, calls countdown and if a negative number is entered, it calls countup funtions respectively

--- UNIT3/assignment/count_down.py
+++ UNIT3/assignment/count_down.py
@@ -1,10 +1,22 @@
 
 def countUp(n):
-    '''Count up from n to 0'''
+    '''Count up from -n to 0 and then print Blast off!'''
     if n == 0:
         print('Blast off!')
     else: 
         print(n)
         countUp(n+1)
-num = int(input('Enter a number: '))
-countUp(num)
+
+def countDown(n):
+    '''Count down from n to 0 and then print Blast off!'''
+    if n >= 0:
+        print(n)
+        countDown(n-1)
+    else:
+        print('Blast off!')
+
+number = int(input('Enter a number: '))
+if number <= 0:
+    countUp(number)
+else:
+    countDown(number)

9b46efa73f183d7c906e6876d80055b5ef013eda
David Nanjila <nanjiladavid2@gmail.com>
2024-04-29 14:22:55 +0300

    a function to divide two numbers

---
+++ UNIT3/assignment/division.py
@@ -0,0 +1,6 @@
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+if num2 == 0:
+    print('Cannot divide by 0')
+else:
+    print(f'{num1} divided by {num2} is {num1/num2}')

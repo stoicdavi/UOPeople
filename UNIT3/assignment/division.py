abf477b025ab269e3d73fb46ebb77b3044883bbc
David Nanjila <nanjiladavid2@gmail.com>
2024-05-01 23:16:45 +0300

    edit code

--- UNIT3/assignment/division.py
+++ UNIT3/assignment/division.py
@@ -2,7 +2,7 @@ while True:
     num1 = int(input('Enter first number: '))
     num2 = int(input('Enter second number: '))
     if num2 == 0:
-        print('Cannot divide by 0')
+        print('Error! Division by zero not allowed!')
     else:
         print(f'{num1} divided by {num2} is {num1/num2}')
     again = input('Do you want to divide again? (yes or no): ')

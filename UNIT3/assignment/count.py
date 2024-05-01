fab0cb68583141b768d025f23db9d622dab1836d
David Nanjila <nanjiladavid2@gmail.com>
2024-05-01 12:14:55 +0300

    Chore: change file name

---
+++ UNIT3/assignment/count.py
@@ -0,0 +1,24 @@
+
+def countUp(n):
+    '''Count up from -n to 0 and then print Blast off!'''
+    if n == 0:
+        print('Blast off!')
+    else: 
+        print(n)
+        countUp(n+1)
+
+def countDown(n):
+    '''Count down from n to 0 and then print Blast off!'''
+    if n >= 0:
+        print(n)
+        countDown(n-1)
+    else:
+        print('Blast off!')
+        
+number = int(input('Enter a number: ')) 
+if number < 0:
+    countUp(number)
+elif number > 0:
+   countDown(number)
+else:
+    print('Invalid input')

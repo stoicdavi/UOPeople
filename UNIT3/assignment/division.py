e51e34d10837939a3487022621f26eb48c5a6d9e
David Nanjila <nanjiladavid2@gmail.com>
2024-05-01 12:24:57 +0300

    feat: add a while loop to allow a user to do as much division as possible and only quit when he/she wants

--- UNIT3/assignment/division.py
+++ UNIT3/assignment/division.py
@@ -1,6 +1,11 @@
-num1 = int(input('Enter first number: '))
-num2 = int(input('Enter second number: '))
-if num2 == 0:
-    print('Cannot divide by 0')
-else:
-    print(f'{num1} divided by {num2} is {num1/num2}')
+while True:
+    num1 = int(input('Enter first number: '))
+    num2 = int(input('Enter second number: '))
+    if num2 == 0:
+        print('Cannot divide by 0')
+    else:
+        print(f'{num1} divided by {num2} is {num1/num2}')
+    again = input('Do you want to divide again? (yes or no): ')
+    if again.lower() != 'yes':
+        break
+print('Goodbye!')

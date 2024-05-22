1d8246c2445b2231b292ef3e6379eca4b874c0d2
David Nanjila <nanjiladavid2@gmail.com>
2024-05-22 12:43:05 +0300

    Create funtion that manipulate lists

---
+++ UNIT6/listcoverter.py
@@ -0,0 +1,9 @@
+instruction = 'The code and its output must be explained technically in a document.'
+dilimeter = ' '
+inst1= instruction.split(dilimeter)
+print(inst1)
+inst2 = ' '.join(inst1)
+print(inst2)
+#reverse instruction 1
+inst1.reverse()
+print(inst1)

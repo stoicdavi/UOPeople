16fe35d8f1cf2b5d05725df80007088e3d9139cb
David Nanjila <nanjiladavid2@gmail.com>
2024-05-24 07:12:02 +0300

    Create a function that alters the list, slices it and ouputs the results

--- UNIT6/listcoverter.py
+++ UNIT6/listcoverter.py
@@ -1,9 +1,11 @@
-instruction = 'The code and its output must be explained technically in a document.'
-dilimeter = ' '
-inst1= instruction.split(dilimeter)
-print(inst1)
-inst2 = ' '.join(inst1)
-print(inst2)
-#reverse instruction 1
-inst1.reverse()
-print(inst1)
+def string_to_list():
+    instruction = 'The code and its output must be explained technically in a document.'
+    dilimeter = ' '
+    inst1= instruction.split(dilimeter)
+    print(inst1)
+    inst2 = ' '.join(inst1)
+    print(inst2)
+    #reverse instruction 1
+    inst1.reverse()
+    print(inst1)
+string_to_list()
